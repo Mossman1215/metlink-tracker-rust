73c8550b1a0fd7694729938a5cc8e81e48c0958c
Moss Cantwell <mossman2142@gmail.com>
2021-01-08 15:33:17 +1300

    brittle parsing

--- metlink-tracker-lib/src/lib.rs
+++ metlink-tracker-lib/src/lib.rs
@@ -1,36 +1,62 @@
-use std::vec;
+use serde_json::{Result, Value};
 
+pub enum VehicleMethod{
+    Bus,
+    Train,
+    Other
+}
 pub struct MetlinkVehicle {
+    pub last_modified: String,
     pub report_time: String,
     pub vehicle_id: String,
     pub service_id: String,
-    pub longitude: f64, 
-    pub latitude: f64,
-    pub bearing: u32,
+    pub longitude: String, 
+    pub latitude: String,
+    pub bearing: String,
     pub late: bool,
     pub service_direction: String,
-    pub destination_id: u32,
-    pub delayed_sec: u64,
+    pub destination_id: String,
+    pub delayed_sec: i64,
+    pub method: VehicleMethod,
 }
 
-pub fn fetch_vehicles_v1(service: String) -> Vec<MetlinkVehicle>{
-    let met_response = reqwest::blocking::get("https://www.metlink.org.nz/api/v1/ServiceLocation/1").unwrap();
+pub fn fetch_vehicles_v1(route: String) -> Vec<MetlinkVehicle>{
+    let url = String::from(format!("{}{}","https://www.metlink.org.nz/api/v1/ServiceLocation/",route.as_str()));
+    let met_response = reqwest::blocking::get(&url).unwrap();
     let body = met_response.text().unwrap();
+
+    let v: Value = serde_json::from_str(&body).unwrap();
+    let timestamp = v["LastModified"].as_str().unwrap();
+    //if v["Services"] > 0
+    let services = v["Services"].as_array().unwrap();
+    let mut vehicles: Vec<MetlinkVehicle> =  Vec::new();
+    if services.len() > 0 {
+        for vehicle_json in services.iter(){
+            let service_json = vehicle_json["Service"].as_object().unwrap();
+            let method_str = service_json["Mode"].as_str().unwrap();
+            let method = match method_str {
+                "Bus" => VehicleMethod::Bus,
+                "Train" => VehicleMethod::Train,
+                _ => VehicleMethod::Other,
+            };
+            vehicles.push(MetlinkVehicle {
+                last_modified: String::from(timestamp),
+                report_time: String::from(vehicle_json["RecordedAtTime"].as_str().unwrap()),
+                vehicle_id: String::from(vehicle_json["VehicleRef"].as_str().unwrap()),
+                service_id: String::from(vehicle_json["ServiceID"].as_str().unwrap()),
+                longitude: String::from(vehicle_json["Long"].as_str().unwrap()),
+                latitude: String::from(vehicle_json["Lat"].as_str().unwrap()),
+                bearing: String::from(vehicle_json["Bearing"].as_str().unwrap()),
+                late: vehicle_json["BehindSchedule"].as_bool().unwrap(),
+                service_direction: String::from(vehicle_json["Bearing"].as_str().unwrap()),
+                destination_id: String::from(vehicle_json["DestinationStopID"].as_str().unwrap()),
+                delayed_sec: vehicle_json["DelaySeconds"].as_i64().unwrap(),
+                method: method,
+            });
+        }
+    }
     
-    println!("body = {:?}", body);
-    let vehicle = MetlinkVehicle {
-        report_time: "some time".to_string(),
-        vehicle_id: "1".to_string(),
-        service_id: "JVL".to_string(),
-        longitude: 1.23,
-        latitude: 201.765,
-        bearing: 100,
-        late: true,
-        service_direction: "Incoming".to_string(),
-        destination_id: 7777,
-        delayed_sec: 100,
-    };
-    vec!(vehicle)
+    vehicles
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,15 @@ use metlink_tracker_lib;
 
 fn main() {
     let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("1"));
-    println!("{}",bus_route_one[0].service_id)
+    for vehicle in bus_route_one.iter() {
+        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
+    }
+    let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("2"));
+    for vehicle in bus_route_one.iter() {
+        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
+    }
+    let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("3"));
+    for vehicle in bus_route_one.iter() {
+        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
+    }
 }
