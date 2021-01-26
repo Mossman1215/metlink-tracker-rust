7b4f9d65839d318ba89b40265cccafcb53f40165
Moss Cantwell <mossman2142@gmail.com>
2021-01-26 22:30:09 +1300

    implemented config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
+//extern crate postgres;
 use metlink_tracker_lib;
+use serde::{Serialize, Deserialize};
+use std::fs;
+//use postgres::{Connection, TlsMode};
 
 fn main() {
-    let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("1"));
-    for vehicle in bus_route_one.iter() {
-        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
+    //fetch list of routes from a toml file?
+    //fetch postgres connection info from toml
+    //fail if toml file is mising
+    //fetch data (iterate over list of routes)
+    //if no param or param = cli print to cli via table?
+    //if param = db
+    // check db or crash 
+    //  push data to postgres db (retry 3x then crash)
+    let conf = load_config();
+    println!("route\tvehicle\tlong\tlat\tbearing");
+    if conf.routes.len()>0 {
+        for route in conf.routes.iter(){
+            let service = metlink_tracker_lib::fetch_vehicles_v1(String::from(route));
+            for vehicle in service.iter() {
+                println!("{}\t{}\t{}\t{}\t{}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
+            }
+        }
     }
-    let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("2"));
-    for vehicle in bus_route_one.iter() {
-        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
-    }
-    let bus_route_one = metlink_tracker_lib::fetch_vehicles_v1(String::from("3"));
-    for vehicle in bus_route_one.iter() {
-        println!("{} {} {} {} {}",vehicle.service_id,vehicle.vehicle_id,vehicle.longitude, vehicle.latitude, vehicle.bearing)
-    }
+}
+
+#[derive(Deserialize)]
+struct Config{
+    hostname: String,
+    port: Option<u16>,
+    routes: Vec<String>,
+}
+fn load_config() -> Config{
+//load file
+let contents = fs::read_to_string("./metlink-tracker.toml")
+        .expect("Something went wrong reading the file");
+//convert to a config struct
+toml::from_str(contents.as_str()).expect("error parsing toml config")
+}
+fn pushtoDB(){
+
 }
